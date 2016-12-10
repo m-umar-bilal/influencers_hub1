74ab9c949eadbc760a7a32e167defb69b45bd46d
UmarBilal <f138190@nu.edu.pk>
2016-12-10 19:43:36 +0500

    Training

---
+++ TextPreProcessing/TextPreProcessing/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TextPreProcessing
+{
+    class Program
+    {
+
+        /* for Categories
+         static void Main(string[] args)
+         {
+
+             var Category = new Category();
+             // Catching exceptions for async tasks
+            Task.Run(async () =>
+             {
+                 try
+                 {
+                     // Start the task.
+                      await Category.InsertCategory("TV & Movies");
+
+                     // Await the task.
+
+                 }
+                 catch (Exception e)
+                 {
+                     Console.WriteLine(e.Message);
+                     // Perform cleanup here.
+                 }
+
+             }).Wait();
+
+             var getListTask = Category.GetAllCategories(); // returns the Task<List<TvChannel>>
+
+             Task.WaitAll(getListTask); // block while the task completes
+
+             var list = getListTask.Result;
+            // Console.WriteLine(list);
+
+
+             foreach (var document in list)
+             {
+                 Console.WriteLine(document);
+             }
+
+
+             //  DataRetrieval d = new DataRetrieval();
+             //  d.proc();
+             //  Console.ReadLine();
+             // String myString;
+             // Console.WriteLine("Enter a string having '&' or '\"'  in it, ");
+             // myString = Console.ReadLine();
+             // myString = "I luv my &lt;3 iphone &amp; @ @umar you’re awsm apple. DisplayIsAwesome, sooo happppppy 🙂 http://www.apple.com”";
+             // Console.WriteLine(myString);
+             //MessageBox.Show(myString);
+             //String myEncodedString = TextCleaner.cleanText(myString);
+             // Console.WriteLine("\n\n" + myEncodedString);
+             // MessageBox.Show(myEncodedString);
+             Console.ReadLine();
+         }*/
+        static void Main(string[] args)
+        {
+            TrainingData c = new TrainingData("TV & Movies");
+          
+            foreach (var s in c.WordCount.Keys)
+            {
+                Console.WriteLine(s+": "+c.WordCount[s]); 
+            }
+            Console.ReadLine();
+        }
+    }
+    }
---
+++ TextPreProcessing/TextPreProcessing/TrainingData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using TextPreProcessing;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using DefaultableDictionary;
+public class TrainingData
+{
+    string content ="";
+    IDictionary<string,int> wordCount = new Dictionary<string, int>().WithDefaultValue(0);
+    string category;
+
+    public IDictionary<string, int> WordCount
+    {
+        get
+        {
+            
+            return wordCount;
+        }
+
+    }
+
+    public TrainingData(string category)
+	{
+        this.category = category;
+        Task.Run(async () =>
+        {
+            try
+            {
+                // Start the task.
+                await MakeTrainingData();
+
+                // Await the task.
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                // Perform cleanup here.
+            }
+
+        }).Wait();
+    }
+    
+    private void InitializeDictionary()
+    {
+     /*   
+        content.Clear();
+        content = new Dictionary<string, string>();
+        var getListTask = Category.GetAllCategories(); // returns the Task<List<TvChannel>>
+
+        Task.WaitAll(getListTask); // block while the task completes
+
+        var list = getListTask.Result;
+        // Console.WriteLine(list);
+
+
+        foreach (var document in list)
+        {
+            content.Add(document, "");
+        }
+      
+    */
+
+    }
+    static string[] SplitWords(string s)
+    {
+      
+        return Regex.Split(s, @"\W+");
+     
+    }
+
+    async Task MakeTrainingData()
+    {
+        wordCount.Clear();
+        wordCount = new Dictionary<string, int>().WithDefaultValue(0);
+     //   InitializeDictionary();
+        var collection = DBConnector.Database.GetCollection<BsonDocument>("TrainingSample");
+        var projection = Builders<BsonDocument>.Projection.Include("tweet").Exclude("_id");
+        var builder = Builders<BsonDocument>.Filter;
+        var filter = builder.Eq("trend_category", category);
+        var options = new FindOptions<BsonDocument> { Projection = projection };
+        var count = 0;
+        using (var cursor = await collection.FindAsync(filter,options))
+        {
+
+            while (await cursor.MoveNextAsync())
+            {
+                var batch = cursor.Current;
+                foreach (var document in batch)
+                {
+                    // process document
+                    content+=TextCleaner.cleanText(document[0].ToString());
+                    count++;
+                }
+            }
+            string[] a = SplitWords(content);
+           foreach (var s in a)
+            {
+                wordCount[s] += 1;
+            }
+          
+        }
+    }
+}
